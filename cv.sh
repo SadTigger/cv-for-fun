85e7b27d661e8b07022824a138515b03b131ed58
Pavel Zhachko <rammiel@yandex.ru>
2016-03-31 15:20:26 +0300

    some version

---
+++ cv.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#cv example
+#author: Pavel Zhacho(Павел Жечко) aka sad_tigger
+#ver 0.1 - 31/03/2016 11.17
+#ver 0.n - 14.45
+
+echo -en "\033[30;1mH̶e̶l̶l̶o̶,̶ ̶w̶o̶r̶d̶!\033[0m"
+
+echo""
+
+echo -en "\033[1;30mJust kidding. 
+Ok, once again."
+
+echo "Hello. My name is Pavel. I'm 23 y-old anykey-junior developer
+with some knowlege in linux and ruby."
+
+echo -en "Well, Here are the skills which I currently possess: 
+
+\033[4;34mOperating systems:\033[0;30m
+	\033[0;32mRFRemix Fedora:\033[1;30m 16 -> 18 -> 20 -> 23
+		Few years as a second OS on my laptop for studying/programming, etc.
+		Gnome 3/Sinnamon fun, but you also need gnome-tools еo change desktop
+		environment for yourself. Dnf > Yum.
+		
+	
+	\033[0;32mArch Linux:\033[1;30m archlinux-2013.11.001 -> archlinux-2014.08.01
+		Another OS for learning programming and doing some of my U works.
+		I know how to use archwiki if I need something.
+		Pacman > Yum. When you need cool desktop monitoring config go to deviantart and 
+		search for conky to your liking
+	
+	\033[0;32mUbuntu:\033[1;30m 12.10 -> 14.04 -> 15.10
+		OS for games and net surfing. Nothing to say. 
+		Ubuntu is mainsteam.(Don't tell that to Ubuntu users)
+
+	p.s. I know how looks execute 'sudo rm -rf*'. It's fun.		
+	
+	\033[0;32mWindows:\033[1;30m 98 -> Xp -> 7 <-> 8/10. Was used as a main OS on different jobs
+	for a few years.
+
+
+
+\033[4;34mProgramming skills:\033[0;30m
+	\033[0;32mC/C++:\033[1;30m
+		Some exp in Borland C++ Builder 6.0; MSVS 2008/2012; cmake, etc
+		Only U works with graph's algorithms. Nothing else.
+	
+	\033[0;32mRuby:\033[1;30m
+		1.9.3 -> 2.0.0
+		Few exp in U works such as http/echo protocols. Rspec? Sometimes.
+		
+	Some basic skills in python/js/java/hashell/as3/pascal"
+	
+	
+	
+
+read answer
